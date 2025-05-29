d15a69567d3fa84ac937eb2a44f747982d861f2e
deandrebaker <deandreb54321@gmail.com>
2025-05-28 19:12:04 -0700

    Add nvim terminal plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -11,4 +11,25 @@ return {
   {
     'github/copilot.vim',
   },
+  {
+    's1n7ax/nvim-terminal',
+    config = function()
+      local Terminal = require 'nvim-terminal.terminal'
+      local Window = require 'nvim-terminal.window'
+      local window = Window:new {
+        position = 'botright',
+        split = 'sp',
+        width = 50,
+        height = 15,
+      }
+
+      terminal = Terminal:new(window)
+      local silent = { silent = true }
+
+      vim.api.nvim_set_keymap('n', '<leader>t', ':lua terminal:toggle()<cr>', silent)
+      vim.api.nvim_set_keymap('n', '<leader>1', ':lua terminal:open(1)<cr>', silent)
+      vim.api.nvim_set_keymap('n', '<leader>2', ':lua terminal:open(2)<cr>', silent)
+      vim.api.nvim_set_keymap('n', '<leader>3', ':lua terminal:open(3)<cr>', silent)
+    end,
+  },
 }
